6ad45db48fcb06b7e031ef7458277f0f19f52c00
Alexander Karan <tech@alexanderkaran.com>
2026-02-07 06:43:40 +0800

    Update types

--- packages/stats-generator/src/ssr/handlers/nextjs.ts
+++ packages/stats-generator/src/ssr/handlers/nextjs.ts
@@ -11,5 +11,6 @@ export async function buildNextJSHandler(): Promise<SSRHandler> {
     dir: join(packagesDir, 'app-next-js'),
   })
   await app.prepare()
-  return app.getRequestHandler() as SSRHandler
+  // TODO: Make the SSRHandler type more flexible so we don't have to cast here
+  return app.getRequestHandler() as unknown as SSRHandler
 }

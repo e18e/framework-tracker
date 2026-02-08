cedab6b96c3cfb190a64ebcade1c5708ca2b524a
Alexander Karan <tech@alexanderkaran.com>
2026-02-08 08:13:39 +0800

    Added TanStack SSR Test

---
+++ packages/app-tanstack-start/src/routes/index.tsx
@@ -0,0 +1,24 @@
+import { createFileRoute } from '@tanstack/react-router'
+import { testData } from '@framework-tracker/testdata'
+
+export const Route = createFileRoute('/')({
+  component: Home,
+  loader: async () => await testData(),
+})
+
+function Home() {
+  const data = Route.useLoaderData()
+
+  return (
+    <table>
+      <tbody>
+        {data.map((entry) => (
+          <tr key={entry.id}>
+            <td>{entry.id}</td>
+            <td>{entry.name}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
---
+++ packages/app-tanstack-start/vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import viteReact from '@vitejs/plugin-react'
+import { nitro } from 'nitro/vite'
+
+export default defineConfig({
+  plugins: [nitro({ preset: 'node-middleware' }), tanstackStart(), viteReact()],
+})

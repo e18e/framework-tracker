4a4781670ffe570f8a36a13cb2f903d5850aa8ff
Tim Neutkens <tim@timneutkens.nl>
2026-02-09 13:02:47 +0100

    Fix Next.js app to match other frameworks (#94)
    * Fix Next.js app to match other frameworks
    
    This is why it's so important to not share benchmark results on social media without verifying you did set up the apps to be a correct comparison of the same application. You set it up to do more work than necessary in Next.js.
    
    * Update table.tsx

---
+++ packages/app-next-js/app/table.tsx
@@ -0,0 +1,17 @@
+'use client'
+import type { TableEntry } from '@framework-tracker/testdata'
+
+export function Table({ data }: { data: TableEntry[] }) {
+  return (
+    <table>
+      <tbody>
+        {data.map((entry) => (
+          <tr key={entry.id}>
+            <td>{entry.id}</td>
+            <td>{entry.name}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}

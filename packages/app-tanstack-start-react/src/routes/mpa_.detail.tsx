869d296597704fa6af199b7a9359128d8a800919
Alexander Karan <tech@alexanderkaran.com>
2026-04-20 13:37:32 +0800

    Format

--- packages/app-tanstack-start-react/src/routes/mpa_.detail.tsx
+++ packages/app-tanstack-start-react/src/routes/mpa_.detail.tsx
@@ -1,15 +1,14 @@
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/mpa_/detail')({
-  loaderDeps: ({ search }) => ({ id: search.id }),
-  loader: ({ deps }) => {
-    return { id: deps.id as string | undefined }
-  },
+  validateSearch: (search) => ({
+    id: typeof search.id === 'string' ? search.id : undefined,
+  }),
   component: MpaDetailPage,
 })
 
 function MpaDetailPage() {
-  const { id } = Route.useLoaderData()
+  const { id } = Route.useSearch()
 
   return <p id="detail-id">{id}</p>
 }

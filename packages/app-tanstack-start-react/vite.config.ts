39729a59760e79f9fd9aba1e3661cfa71201b23e
Alexander Karan <tech@alexanderkaran.com>
2026-04-24 10:29:43 +0800

    SPA Mode Fails

--- packages/app-tanstack-start-react/vite.config.ts
+++ packages/app-tanstack-start-react/vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import { nitro } from 'nitro/vite'
+import { defineConfig } from 'vite'
 
 const isSpa = process.env.BUILD_MODE === 'spa'
 
 export default defineConfig({
+  preview: {
+    host: '127.0.0.1',
+  },
   plugins: [
     ...(isSpa ? [] : [nitro({ preset: 'node-middleware' })]),
     tanstackStart({ spa: { enabled: isSpa } }),

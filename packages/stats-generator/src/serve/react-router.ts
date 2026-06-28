81f82af3e9ec57c620bfe00bfebc21bf37940f29
Alexander Karan <tech@alexanderkaran.com>
2026-06-28 20:25:30 +0800

    Few more cleanups after merging all other PRs

--- packages/stats-generator/src/serve/react-router.ts
+++ packages/stats-generator/src/serve/react-router.ts
@@ -1,7 +1,6 @@
-import { spawn } from 'node:child_process'
 import { createRequire } from 'node:module'
 import { dirname, join } from 'node:path'
-import { getPort, parseAppDir } from './common.ts'
+import { getPort, parseAppDir, spawnProductionServer } from './common.ts'
 
 const appDir = parseAppDir()
 const PORT = getPort()
@@ -11,23 +10,6 @@ const appRequire = createRequire(join(appDir, 'package.json'))
 const servePackagePath = appRequire.resolve('@react-router/serve/package.json')
 const serveBinPath = join(dirname(servePackagePath), 'bin.js')
 
-const child = spawn(process.execPath, [serveBinPath, buildPath], {
-  cwd: appDir,
-  env: {
-    ...process.env,
-    NODE_ENV: 'production',
-    PORT: String(PORT),
-  },
-  stdio: 'inherit',
-})
-
-const shutdown = () => {
-  child.kill('SIGTERM')
-}
-
-process.on('SIGTERM', shutdown)
-process.on('SIGINT', shutdown)
-
-child.on('exit', (code) => {
-  process.exit(code ?? 0)
+spawnProductionServer([serveBinPath, buildPath], appDir, {
+  PORT: String(PORT),
 })

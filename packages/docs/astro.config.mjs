c4906b8eb15c0c60debe7c8413e438cd9c10a54b
James Garbutt <43081j@users.noreply.github.com>
2026-07-22 10:08:12 +0100

    feat: rework deployment to be a worker (#313)
    This moves away from cloudflare pages and uses a worker instead - just
    static contents.
    
    It also changes the domain in preparation for being bound to it.

--- packages/docs/astro.config.mjs
+++ packages/docs/astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://framework-tracker.pages.dev',
+  site: 'https://frameworks.e18e.dev',
   integrations: [
     starlight({
       title: 'Framework Tracker',

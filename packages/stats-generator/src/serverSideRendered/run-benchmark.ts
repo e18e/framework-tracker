f66e0e090c769ece5a863017c2e82a4ce3174710
Alexander Karan <47707063+AlexanderKaran@users.noreply.github.com>
2026-08-09 21:19:28 +0800

    Revert "Temp Commit to Test"
    This reverts commit 51677dd9481163fd3b6a2c77efb02711d5c1bf30.

--- packages/stats-generator/src/serverSideRendered/run-benchmark.ts
+++ packages/stats-generator/src/serverSideRendered/run-benchmark.ts
@@ -6,7 +6,6 @@ import {
   getInteractionTimingFromLighthouse,
   INTERACTION_SCENARIO,
   INTERACTION_SOURCE,
-  logInteractionTrace,
 } from '../interaction-timing.ts'
 import type { InteractionTestStats } from '../types.ts'
 import type {
@@ -105,9 +104,6 @@ async function runOnce(
       await flow.endTimespan()
     }
 
-    const interactionTrace = fullDocumentNavigation
-      ? flow.createArtifactsJson().gatherSteps[1]?.artifacts.Trace
-      : undefined
     const flowResult = await flow.createFlowResult()
     const navLhr = flowResult.steps[0].lhr
     const interactionLhr = flowResult.steps[1].lhr
@@ -120,10 +116,6 @@ async function runOnce(
     const interaction = getInteractionTimingFromLighthouse(
       interactionLhr.audits['inp-breakdown-insight'],
     )
-
-    if (fullDocumentNavigation) {
-      logInteractionTrace(interactionTrace, interaction !== null)
-    }
 
     await page.close()
     return { firstPaintMs, fcpMs, interaction }

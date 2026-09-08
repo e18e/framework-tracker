f27f34030e77ae8a957bf11929498f8e0caf3439
Alexander Karan <47707063+AlexanderKaran@users.noreply.github.com>
2026-09-08 21:16:12 +0800

    React Router Sign Off (#455)

--- packages/app-react-router/react-router.config.ts
+++ packages/app-react-router/react-router.config.ts
@@ -2,4 +2,11 @@ import type { Config } from '@react-router/dev/config'
 
 export default {
   ssr: true,
+  future: {
+    v8_middleware: true,
+    v8_splitRouteModules: true,
+    v8_viteEnvironmentApi: true,
+    // v8_passThroughRequests: true, // Supported from React Router 7.15
+    // v8_trailingSlashAwareDataRequests: true, // Supported from React Router 7.16
+  },
 } satisfies Config

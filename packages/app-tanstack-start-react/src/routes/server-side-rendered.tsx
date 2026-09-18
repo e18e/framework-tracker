63472131c2992f308607cb6ce8b468aae3a401a9
Alexander Karan <47707063+AlexanderKaran@users.noreply.github.com>
2026-09-18 21:56:13 +0800

    Moved location of load for test data (#483)

--- packages/app-tanstack-start-react/src/routes/server-side-rendered.tsx
+++ packages/app-tanstack-start-react/src/routes/server-side-rendered.tsx
@@ -1,9 +1,9 @@
 import { createFileRoute, Link } from '@tanstack/react-router'
-import { testData } from '../../../testdata/src/ssr'
+import { getTestData } from '../data/test-data'
 
 export const Route = createFileRoute('/server-side-rendered')({
   component: ServerSideRenderedPage,
-  loader: async () => await testData(),
+  loader: () => getTestData(),
 })
 
 function ServerSideRenderedPage() {

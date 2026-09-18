25899da606f7c8b82c82edfb034c27dddc26b03b
Alexander Karan <47707063+AlexanderKaran@users.noreply.github.com>
2026-09-18 14:57:53 +0800

    Remove Context

--- packages/app-tanstack-start-react/src/router.tsx
+++ packages/app-tanstack-start-react/src/router.tsx
@@ -4,7 +4,6 @@ import { routeTree } from './routeTree.gen'
 export const getRouter = () => {
   const router = createRouter({
     routeTree,
-    context: {},
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
     defaultPendingMinMs: 0,

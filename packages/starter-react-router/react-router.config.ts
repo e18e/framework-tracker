9708bd409e2539206acab53b039bf8e4220c5ae5
Alexander Karan <47707063+AlexanderKaran@users.noreply.github.com>
2026-09-25 12:50:09 +0800

    Bumped React Router Version

--- packages/starter-react-router/react-router.config.ts
+++ packages/starter-react-router/react-router.config.ts
@@ -8,7 +8,7 @@ export default {
     v8_middleware: true,
     v8_splitRouteModules: true,
     v8_viteEnvironmentApi: true,
-    // v8_passThroughRequests: true, // Supported from React Router 7.15
+    v8_passThroughRequests: true,
     // v8_trailingSlashAwareDataRequests: true, // Supported from React Router 7.16
   },
 } satisfies Config
